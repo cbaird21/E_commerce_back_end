644fe42876c833c89b25577d2808a7db1727d47b
unknown <chandler.baird21@gmail.com>
2022-12-07 13:36:19 -0700

    added index.js

--- models/index.js
+++ models/index.js
@@ -3,16 +3,23 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const { DataTypes } = require('sequelize/types');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+})
 
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+})
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, { through: 'ProductTag' });
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, { through: 'ProductTag' })
 
 module.exports = {
   Product,
