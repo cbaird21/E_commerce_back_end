2cd72bcc1156953e670d758a301d41eb839e00f8
unknown <chandler.baird21@gmail.com>
2022-12-07 09:11:18 -0700

    inital tag routes attempts

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,26 +3,54 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
+  const ProductTag = await User.findAll().catch((err) => {
+    res.json(err);
+  });
+  res.json(ProductTag);
   // be sure to include its associated Product data
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
+  const productData = await User.findByPk(req.params.id).catch((err) =>
+    res.json(err)
+  );
+  res.json(productData);
   // be sure to include its associated Product data
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const userData = await User.create(req.body);
+    // 200 status code means the request is successful
+    res.status(200).json(userData);
+  } catch (err) {
+    // 400 status code means the server could not understand the request
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  const productData = await User.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  }).catch((err) => res.json(err));
+  res.json(productData);
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  const productData = await User.destroy({
+    where: {
+      id: req.params.id,
+    },
+  }).catch((err) => res.json(err));
+  res.json(productData);
 });
 
 module.exports = router;
